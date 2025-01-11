0c4e862c2f5a39710e6112d0ad73aa6347c4c93f
Amena Akter <amenaakterkeya31@gmail.com>
2025-01-11 22:10:10 +0600

    can place flowers

---
+++ 605. Can Place Flowers.py
@@ -0,0 +1,13 @@
+#problem link: https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        length = len(flowerbed)
+        for i in range(length):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == length - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                count += 1
+                if count >= n:
+                    return True
+        return count >= n
